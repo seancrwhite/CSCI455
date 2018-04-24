7c7018a694f2ffbd0bdd82a26a878077d38a9b93
Sean White <seancrwhite@gmail.com>
2018-04-24 12:28:48 -0600

    Android app does not take input after game over anymore

--- RobotVocalController/app/src/main/java/csci455/seancrwhite/robotvocalcontroller/MainActivity.java
+++ RobotVocalController/app/src/main/java/csci455/seancrwhite/robotvocalcontroller/MainActivity.java
@@ -96,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            if(phrase.contains("Game over")) {
+                return;
+            }
+
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
